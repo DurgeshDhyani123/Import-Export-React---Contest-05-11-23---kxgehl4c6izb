a9ac7078463540e8ac10207ec6ecb95fa6618982
DurgeshDhyani123 <49858476+DurgeshDhyani123@users.noreply.github.com>
2023-11-05 11:41:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,61 @@
+import export react:
+App.js:
 import React, { Component, useState } from "react";
-import React from 'react';
-import Heading from './components/Heading';
-import SubHeading from './components/SubHeading';
-import InputQuery from './components/InputQuery';
-import SubmitButton from './components/SubmitButton';
-
 import '../styles/App.css';
+import Heading from './Heading.js';
+import SubHeading from './SubHeading.js';
+import InputQuery from './InputQuery.js';
+import SubmitButton from './SubmitButton.js';
 
 const App = () => {
   return (
     <div id="main">
-         <Heading />
+      <Heading />
       <SubHeading />
       <InputQuery />
       <SubmitButton />
     </div>
   )
 }
+
 export default App;
 
+Heading.js:
+import React from 'react'
 
+function Heading() {
+    return (
+        <h1>Welcome to our Site.</h1>
+    )
+}
+export default Heading;
+
+InputQuery.js:
+import React from 'react'
+
+function InputQuery() {
+    return (
+        <input placeHolder={'Enter your query here..'} />
+    )
+}
+export default InputQuery;
+
+SubHeading.js:
+import React from 'react'
+
+function SubHeading() {
+    return (
+        <h5>Happy to solve you doubts.</h5>
+    )
+}
+export default SubHeading;
+
+SubmitButton.js:
+import React from 'react'
+
+function SubmitButton() {
+    return (
+        <button>Ask</button>     
+    )
+}
+export default SubmitButton;
